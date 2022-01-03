7e7e7d1270da91fc03e53801d3d49d1fae6b03f8
Antoine Fuchs <antoine-fuchs@live.de>
2022-01-03 11:23:27 +0100

    Search optimiert

--- .history/Website/Backend_20220103112229.js
+++ .history/Website/Backend_20220103112229.js
@@ -119,7 +119,24 @@ function search_and_display()
                 dat.push(data[i].jsonstring);
                 var heading1 = document.getElementById("heading1");
                 heading1.firstChild.nodeValue = dat[0].Titel;
-                
+                <div class="container-md">
+                <div class="row">
+                  <div class="col">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                            <ul class="list-group list-group-flush">
+                              <li class="list-group-item">Adress</li>
+                              <li class="list-group-item">Cost / Pricing</li>
+                            </ul>  
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                  </div>
+                  </div>
+                  </div>
             }
          
         };
