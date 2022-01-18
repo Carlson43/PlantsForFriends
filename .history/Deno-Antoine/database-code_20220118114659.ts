b3a37c9c195b4190023971ca78962019eb231a4f
antoine-fuchs <94910891+antoine-fuchs@users.noreply.github.com>
2022-01-18 11:53:30 +0100

    Database update

--- .history/Deno-Antoine/database-code_20220118114659.ts
+++ .history/Deno-Antoine/database-code_20220118114659.ts
@@ -6,9 +6,11 @@ console.log(projectPathForData)
 
 /*************      Remote Mode - using GitHub as database      *************/
 const userName = "plants-for-friends"
+
+
 const password = 'dreadful-fame9-presuming'
 const org = "plants-for-friends"
-const repo = "PlantsForFriends"
+const repo = "plants-for-friends"
 await Persistence.commitAndPush(projectPathForData, userName, password, org, repo) // commented out as I should not publish my password
 
 const linkToExampleDataRemote = "https://raw.githubusercontent.com/Carlson43/PlantsForFriends/blob/Web-Dev/Deno-Antoine/database.json"
