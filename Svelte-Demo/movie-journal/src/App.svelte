fbb5276de2adf5c697694632902661511433b0ed
Carlson43 <58775878+Carlson43@users.noreply.github.com>
2022-01-18 14:00:06 +0100

    Files for Svelte Tutorial

---
+++ Svelte-Demo/movie-journal/src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+import { detach } from 'svelte/internal';
+
+	import MovieInput from './MovieInput.svelte';
+	export let name;
+	export let newProp;
+
+	let movies = localStorage.getItem('movies') ?
+		JSON.parse(localStorage.getItem('movies')) :
+		[];
+
+	const submitMovie = movie => {
+    	const updatedMovies = [ ...movies, movie ];
+    	localStorage.setItem('movies', JSON.stringify(updatedMovies));
+    	movies = updatedMovies;
+  	}
+</script>
+
+<dir class="main">
+	<h1>Movie Journal</h1>
+
+	<MovieInput on:submitMovie={event => submitMovie(event.detail.movie)} />
+</dir>
+
+<style>
+	.main{
+
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
