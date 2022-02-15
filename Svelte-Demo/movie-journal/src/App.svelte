fa1832538d59c0cdeeb7ba9bc505d32023c39890
Daraen04 <84714614+Daraen04@users.noreply.github.com>
2022-02-15 21:14:26 +0100

    datenbank kp was passiert

---
+++ Svelte-Demo/movie-journal/src/App.svelte
@@ -0,0 +1,67 @@
+<script>
+	import MovieInput from './MovieInput.svelte';
+	import MovieList from './MovieList.svelte';
+	import Search from './Search.svelte';
+
+
+	let movies = localStorage.getItem('movies') ? 
+	  JSON.parse(localStorage.getItem('movies')) : 
+	  [];
+
+	const submitMovie = movie => {
+	  const updatedMovies = [ ...movies, movie ];
+	  localStorage.setItem('movies', JSON.stringify(updatedMovies));
+	  movies = updatedMovies;
+	}
+
+	const clearSearch = () => {
+	  movies = localStorage.getItem('movies') ? 
+		JSON.parse(localStorage.getItem('movies')) : 
+		[];
+	};
+
+	const search = searchTerm => {
+	  const tempMovies = localStorage.getItem('movies') ? 
+		JSON.parse(localStorage.getItem('movies')) : 
+		[];
+   
+	  movies = tempMovies.filter(m => 
+		m.title.toLowerCase().includes(searchTerm.toLowerCase()));
+	};
+
+// const sqlite3 = require('sqlite3')
+// const db = new sqlite3.Database('contacts.db')
+
+
+console.log('Läuft durch')
+
+
+
+
+
+  </script>
+  
+  <div class='main'>
+	<h1>Movie Journal</h1>
+  
+	<Search on:search={event => search(event.detail.searchTerm)} on:clearSearch={clearSearch} />
+	<MovieInput on:submitMovie={event => submitMovie(event.detail.movie)} />
+	<MovieList movies={movies} />
+  </div>
+  
+  <style>
+	  .main {
+	  width: 500px;
+	  max-width: 100%;
+	  padding: 1em;
+	  margin: auto;
+	  text-align: center;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+  </style>
