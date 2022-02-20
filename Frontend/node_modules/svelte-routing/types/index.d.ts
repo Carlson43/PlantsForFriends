b6f161a538a27a8a702aeeae89dfa823fad6ea81
antoine-fuchs <94910891+antoine-fuchs@users.noreply.github.com>
2022-02-20 13:12:15 +0100

    Delete Frontend directory
    not needed

--- Frontend/node_modules/svelte-routing/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { Router } from './Router';
-export { Route } from './Route';
-export { Link } from './Link';
-export { link, links } from './actions';
-export { navigate } from './functions';
