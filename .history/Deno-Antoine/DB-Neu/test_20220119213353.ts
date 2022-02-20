8fa5ed8b63fb3058794b1f8461ecf21896776d0f
antoine-fuchs <94910891+antoine-fuchs@users.noreply.github.com>
2022-02-20 13:13:24 +0100

    Delete .history directory
    not needed

--- .history/Deno-Antoine/DB-Neu/test_20220119213353.ts
+++
@@ -1,8 +0,0 @@
-//const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://Test:uTyAZp11kktRh3YH@cluster0.jjpui.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
