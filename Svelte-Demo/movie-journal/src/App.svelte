df0df38744937919dc1f93e7c1718b6dc6fca635
Carlson43 <58775878+Carlson43@users.noreply.github.com>
2022-02-20 12:13:20 +0100

    aded comments

--- Svelte-Demo/movie-journal/src/App.svelte
+++ Svelte-Demo/movie-journal/src/App.svelte
@@ -3,40 +3,49 @@
 	import MovieList from './MovieList.svelte';
 	import Search from './Search.svelte';
 
+	// create local storage
 	let movies = localStorage.getItem('movies') ? 
 	  JSON.parse(localStorage.getItem('movies')) : 
 	  [];
 
+	// add new entrie
 	const submitMovie = movie => {
 	  const updatedMovies = [ ...movies, movie ];
 	  localStorage.setItem('movies', JSON.stringify(updatedMovies));
 	  movies = updatedMovies;
 	}
 
+	// reset movies when search is cleared
 	const clearSearch = () => {
 	  movies = localStorage.getItem('movies') ? 
 		JSON.parse(localStorage.getItem('movies')) : 
 		[];
 	};
 
+	// search
+	// Create copy of all entries
 	const search = searchTerm => {
 	  const tempMovies = localStorage.getItem('movies') ? 
 		JSON.parse(localStorage.getItem('movies')) : 
 		[];
    
+	// Search trough copy with filter method
 	  movies = tempMovies.filter(m => 
 		m.title.toLowerCase().includes(searchTerm.toLowerCase()));
 	};
   </script>
   
+  <!-- Create main body of application -->
   <div class='main'>
 	<h1>Movie Journal</h1>
   
+	<!-- external Components -->
 	<Search on:search={event => search(event.detail.searchTerm)} on:clearSearch={clearSearch} />
 	<MovieInput on:submitMovie={event => submitMovie(event.detail.movie)} />
 	<MovieList movies={movies} />
   </div>
   
+  <!-- Sytling -->
   <style>
 	  .main {
 	  width: 500px;
